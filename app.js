412b9577f2b6ffbc9c08585198107cb22ac00edd
Vivek Mittal <42748450+vivek1909@users.noreply.github.com>
2019-07-30 23:28:21 +0530

    added onclick functionality to buttons

--- app.js
+++ app.js
@@ -12,18 +12,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 
@@ -41,10 +30,31 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-        // Next Player
+        nextPlayer();
+    }
+
+
+    document.querySelector('.btn-hold').addEventListener('click', function() {
+        // Add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
+
+        // Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        // Check if player has won the game
+        if(scores[activePlayer] >= 100) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        } else {
+            nextPlayer();
+        }
+    });
+
+    function nextPlayer() {
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore = 0;
-
 
         document.getElementById('current-0').textContent = '0';
         document.getElementById('current-1').textContent = '0';
@@ -53,5 +63,28 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('.player-1-panel').classList.toggle('active');
 
         document.querySelector('.dice').style.display = 'none';
+    }
+
+    document.querySelector('.btn-new').addEventListener('click', init);
+
+    function init() {
+        scores = [0, 0];
+        roundScore = 0;
+        activePlayer = 0;
+
+        document.querySelector('.dice').style.display = 'none';
+
+        document.getElementById('score-0').textContent = '0';
+        document.getElementById('score-1').textContent = '0';
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        document.getElementById('name-0').textContent = 'Player 1';
+        document.getElementById('name-1').textContent = 'Player 2';
+        
+        document.querySelector('.player-0-panel').classList.remove('winner');
+        document.querySelector('.player-1-panel').classList.remove('winner');
+        document.querySelector('.player-0-panel').classList.remove('active');
+        document.querySelector('.player-1-panel').classList.remove('active');
+        document.querySelector('.player-0-panel').classList.add('active');
     }
 });
